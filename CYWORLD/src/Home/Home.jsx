5894bcf4d85e1cd019d594a6582e73fbe2825572
이재호 <111284065+jaeho13@users.noreply.github.com>
2023-07-04 22:21:47 +0900

    Add files via upload

--- CYWORLD/src/Home/Home.jsx
+++ CYWORLD/src/Home/Home.jsx
@@ -1,27 +1,41 @@
 import React from "react";
-import styled from "styled-components"
+import styled from "styled-components";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const Home = () => {
     return (
-        <>
-            <Body
-            // img src="/background/pattern.png" alt="배경"
-            >
+        <Body>
+            <BookCover>
+                <Bookdot>
+                    <Page>
+                        <Container1>
+                            <Item1>
+                                <Box>TODAY 28 | TOTAL 123456</Box>
+                            </Item1>
+                            <Item2></Item2>
+                        </Container1>
 
-                <BookCover>안녕하세요
-                    <Bookdot>위치가 어딜까요
-                        <Page>이건 어디야</Page>
-                    </Bookdot>
-                </BookCover>
-            </Body>
-
-        </>
+                        <Container2>
+                            <Item3>
+                                이재호의 포트폴리오
+                                <Tab>
+                                    <a href="https://github.com/jaeho13">
+                                        https://github.com/jaeho13
+                                    </a>
+                                </Tab>
+                            </Item3>
+                            <Item4></Item4>
+                        </Container2>
+                        <Container3></Container3>
+                    </Page>
+                </Bookdot>
+            </BookCover>
+        </Body>
     );
 };
 
-export default Home;
-
 const Body = styled.div`
+    font-size: 16px; /* 예시로 16px을 기준으로 설정 */
     background-color: #a3a3a3;
     background-image: url(/background/pattern.png);
     background-size: 100px;
@@ -30,40 +44,104 @@ const Body = styled.div`
     top: 0;
     right: 0;
     bottom: 0;
-`
+`;
 
 const BookCover = styled.div`
-    width: 70%;
-    height: 45rem;
-    border: 1px solid #738186;
-    border-radius: 0.5rem;
     background-color: #b4d1da;
+    border-radius: 1rem;
+    border: 1px solid #738186;
+    width: 70rem;
+    height: 45rem;
     margin: 0 auto;
-    margin-top: 5rem;
+    margin-top: 7rem;
     margin-bottom: 5rem;
-   /** .bookdot 요소에서 absolute 속성값을 이용하기 위해 position: relative 로 설정 */
     position: relative;
-`
+`;
 
 const Bookdot = styled.div`
-    border: 3px dashed white;
-    border-radius: 9px;
-    left: 15px;
-    top: 15px;
-    right: 15px;
-    bottom: 15px;
+    border: 0.2rem dashed white;
+    border-radius: 0.9rem;
+    left: 1.5rem;
+    top: 1.5rem;
+    right: 1.5rem;
+    bottom: 1.5rem;
     position: absolute;
-`
+`;
 
 const Page = styled.div`
-    width: 60rem;
-    height: 40rem;
-    border-radius: 0.5rem;
-    background-color: white;
-    margin: auto; /* 가로 중앙 정렬 */
-    position: absolute;
-    top: 0;
-    left: 0;
-    right: 0;
-    bottom: 0;
-`
+  background-color: #d4d4d4;
+  border-radius: 0.9rem;
+  width: 65rem;
+  height: 40rem;
+  margin: 1rem;
+  position: absolute;
+  display: flex;
+`;
+
+const Container1 = styled.div`
+  flex: 0.4;
+  display: flex;
+  flex-direction: column;
+  margin-left: 1rem;
+  margin-right: 1rem;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+`;
+
+const Item1 = styled.div`
+  flex: 0.1;
+  border: none;
+  padding: 0.5rem;
+`;
+
+const Item2 = styled.div`
+  flex: 2;
+  border: 0.2rem solid #d4d4d4;
+  border-radius: 1rem;
+  background-color: white;
+`;
+
+const Container2 = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  margin-left: 1rem;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+`;
+
+const Item3 = styled.div`
+  flex: 0.1;
+  border: none;
+  padding: 0.5rem;
+`;
+
+const Item4 = styled.div`
+  flex: 2;
+  border: 0.2rem solid #d4d4d4;
+  border-radius: 1rem;
+  background-color: white;
+`;
+
+const Container3 = styled.div`
+  flex: 0.1;
+  border: 0.1rem solid grey;
+  margin-right: 1rem;
+  margin-top: 1rem;
+  margin-bottom: 1rem;
+  margin-left: 1rem;
+`;
+
+const Box = styled.div`
+  display: inline-block;
+  text-align: center;
+  font-size: 1rem;
+  margin-left: 1.5rem;
+`;
+
+const Tab = styled.div`
+  display: inline-block;
+  margin-left: 5rem;
+`;
+
+export default Home;
