f97c3610a749d5cab8df692a09189aba3c9307c3
이재호 <111284065+jaeho13@users.noreply.github.com>
2023-07-07 01:30:09 +0900

    Add files via upload

--- CYWORLD/src/Home/Self.jsx
+++ CYWORLD/src/Home/Self.jsx
@@ -9,19 +9,24 @@ const Self = () => {
                     <Page>
                         <Main>
                             <Bind>
-                                <Visit>방문자수 123명</Visit>
-                                <HomePage>이재호의 미니홈피</HomePage>
-                                <Link>이 홈페이지 주소</Link>
+                                <Visit>TODAY <Visit2>28</Visit2> | TOTAL 123456</Visit>
+                                <Bind2>
+                                    <HomePage>이재호의 미니홈피</HomePage>
+                                    <Link>https://github.com/jaeho13</Link>
+                                </Bind2>
                             </Bind>
 
-                            <Bind2>
+                            <Bind3>
                                 <Left>
                                     <LeftTop>사진</LeftTop>
                                     <LeftBottom>스크롤</LeftBottom>
                                 </Left>
                                 <Center>
-                                    <CentetTop>깃허브 그것처럼 기술 간단하게</CentetTop>
-                                    <CentetBottom>공부 사진</CentetBottom>
+                                    <CentetTop>Stack</CentetTop>
+                                    <CentetMiddle>Miniroom</CentetMiddle>
+                                    <CenterBottom>
+                                        <CenterBottomImage src="/background/Study.png" alt="study" />
+                                    </CenterBottom>
                                 </Center>
                                 <Right>
                                     <RightButton1>Home</RightButton1>
@@ -29,7 +34,7 @@ const Self = () => {
                                     <RightButton3>Project</RightButton3>
                                     <RightButton4>Contact</RightButton4>
                                 </Right>
-                            </Bind2>
+                            </Bind3>
                         </Main>
                     </Page>
                 </BookCover>
@@ -85,10 +90,18 @@ const Main = styled.div`
 const Bind = styled.div`
     display: flex;
     flex-direction: row;
-    margin-top: 0.8rem;
+    /* margin-top: 0.8rem; */
 `
 
 const Bind2 = styled.div`
+    width: 70%;
+    display: flex;
+    flex-direction: row;
+    margin-top: 1rem;
+    /* justify-content: space-between; */
+`
+
+const Bind3 = styled.div`
     display: flex;
     flex-direction: row;
 `
@@ -98,72 +111,117 @@ const Visit = styled.div`
     overflow: hidden;
     text-overflow: ellipsis;
     width: 20%;
-    height: 3rem;
-    border: 2px solid blue;
+    height: 1.5rem;
+    /* border: 2px solid blue; */
     margin-left: 2rem;
-    font-size: 2rem;
+    font-size: 1.2rem;
+    padding-top: 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 `
+
+const Visit2 = styled.div`
+    color: red;
+    margin-left: 0.5rem;
+    margin-right: 0.5rem;
+`
+
 
 const HomePage = styled.div`
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
-    width: 20%;
-    height: 3rem;
-    border: 2px solid black;
-    margin-left: 3rem;
-    font-size: 2rem;
+    width: 80%;
+    height: 2rem;
+    /* border: 2px solid black; */
+    margin-left: 4rem;
+    font-size: 1.5rem;
 `
 
 const Link = styled.div`
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
-    width: 30%;
-    height: 3rem;
-    border: 2px solid green;
-    margin-left: 3rem;
-    font-size: 2rem;
+    width: 80%;
+    height: 2rem;
+    /* border: 2px solid green; */
+    margin-left: 10rem;
+    margin-right: 6rem;
+    font-size: 1.15rem;
+    display: flex;
+    justify-content: right;
+    align-items: center;
+    color: grey;
 `
 
 const Left = styled.div`
     width: 20%;
-    height: 33rem;
-    border: 2px solid red;
-    margin-top: 2rem;
+    height: 41rem;
+    /* border: 2px solid red; */
+    margin-top: 0.1rem;
     margin-left: 2rem;
+    background-color: rgba(255, 255, 255, 0.6);
+    border-radius: 1rem;
 `
 
 const LeftTop = styled.div`
     width: 100%;
     height: 20rem;
-    border: 2px solid green;
+    /* border: 2px solid green; */
 `
 
 const LeftBottom = styled.div`
     width: 100%;
-    height: 10rem;
-    border: 2px solid black;
+    height: 19rem;
+    /* border: 2px solid black; */
 `
 
 const Center = styled.div`
     width: 60%;
-    height: 33rem;
-    border: 2px solid purple;
-    margin-top: 2rem;
+    height: 40.7rem;
+    /* border: 2px solid purple; */
+    margin-top: 0.5rem;
     margin-left: 3rem;
+    background-color: rgba(255, 255, 255, 0.6);
+    border-radius: 1rem;
 `
 
 const CentetTop = styled.div`
     width: 100%;
-    height: 15rem;
-    border: 2px solid black;
+    height: 14.5rem;
+    margin-left: 1.3rem;
+    font-size: 1.2rem;
+    margin-top: 0.5rem;
+    color: #3B87AB;
+    font-weight: bold;
+    /* border: 2px solid black; */
 `
 
-const CentetBottom = styled.div`
+const CentetMiddle = styled.div`
     width: 100%;
-    height: 15rem;
-    border: 2px solid pink;
+    height: 1.2rem;
+    font-size: 1.2rem;
+    margin-left: 1.3rem;
+    font-weight: bold;
+    color: #3B87AB;
+`
+
+const CenterBottom = styled.div`
+    width: 95%;
+    height: 19rem;
+    /* border: 2px solid pink; */
+    display: flex;
+    margin: 0 auto;
+    margin-top: 1rem;
+    /* align-items: center;
+    justify-content: center; */
+`
+
+const CenterBottomImage = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
 `
 
 const Right = styled.div`
@@ -177,7 +235,7 @@ const Right = styled.div`
 const RightButton1 = styled.button`
     width: 100%;
     height: 3rem;
-    border: 2px solid black;
+    border: 2px solid grey;
     border-top-right-radius: 0.5rem;
     border-bottom-right-radius: 0.5rem;
     margin-top: 1rem;
@@ -196,7 +254,7 @@ const RightButton1 = styled.button`
 const RightButton2 = styled.button`
     width: 100%;
     height: 3rem;
-    border: 2px solid black;
+    border: 2px solid grey;
     border-top-right-radius: 0.5rem;
     border-bottom-right-radius: 0.5rem;
     margin-top: 1rem;
@@ -215,7 +273,7 @@ const RightButton2 = styled.button`
 const RightButton3 = styled.button`
     width: 100%;
     height: 3rem;
-    border: 2px solid black;
+    border: 2px solid grey;
     border-top-right-radius: 0.5rem;
     border-bottom-right-radius: 0.5rem;
     margin-top: 1rem;
@@ -234,7 +292,7 @@ const RightButton3 = styled.button`
 const RightButton4 = styled.button`
     width: 100%;
     height: 3rem;
-    border: 2px solid black;
+    border: 2px solid grey;
     border-top-right-radius: 0.5rem;
     border-bottom-right-radius: 0.5rem;
     margin-top: 1rem;
