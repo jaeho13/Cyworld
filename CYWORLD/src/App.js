692618a77abe2cebbabe3b88a8f8e376f898c177
jaeho13 <jaehooo13@naver.com>
2023-10-12 02:06:11 +0900

    1012 jaeho 코드 리뷰 및 수정

--- CYWORLD/src/App.js
+++ CYWORLD/src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import Profile from "./Components/Profile";
 import Project from "./Components/Project";
@@ -8,14 +8,14 @@ import Contact from "./Components/Contact";
 function App() {
   return (
     <>
-      <Router>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/project" element={<Project />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </Router>
+      </BrowserRouter>
     </>
   );
 }
