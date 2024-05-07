908868edd965de7bb122bdc8549602bd23a8b32f
jaeho13 <jaehooo13@naver.com>
2024-05-07 23:54:18 +0900

    "0507 버튼 컴포넌트화 테스트"

---
+++ CYWORLD/src/Components/button/Button.jsx
@@ -0,0 +1,99 @@
+import styled from "styled-components";
+
+const Button = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+export default Button;
+
+
+const RightHomeFlex = styled.div`
+    width: 97%;
+    height: 5vh;
+    border: 2px solid grey;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+    cursor: pointer;
+    color: black;
+    background-color: white;
+    margin-top: 5vh;
+    padding-left: 3%;
+    display: flex;
+    align-items: center;
+`
+
+const RightHome = styled.div`
+    font-size: 1.5rem;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+`
+
+const RightProfileFlex = styled.div`
+    width: 97%;
+    height: 5vh;
+    border: 2px solid grey;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+    cursor: pointer;    
+    color: white;
+    background-color: #3B87AB;
+    margin-top: 1vh;
+    padding-left: 3%;
+    display: flex;
+    align-items: center;
+`
+
+const RightProfile = styled.div`
+    font-size: 1.5rem;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+`
+
+const RightProjectFlex = styled.div`
+    width: 97%;
+    height: 5vh;
+    border: 2px solid grey;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+    cursor: pointer;
+    color: white;
+    background-color: #3B87AB;
+    margin-top: 1vh;
+    padding-left: 3%;
+    display: flex;
+    align-items: center;
+`
+
+const RightProject = styled.div`
+    font-size: 1.5rem;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+`
+
+const RightContactFlex = styled.div`
+    width: 97%;
+    height: 5vh;
+    border: 2px solid grey;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+    cursor: pointer;
+    color: white;
+    background-color: #3B87AB;
+    margin-top: 1vh;
+    padding-left: 3%;
+    display: flex;
+    align-items: center;
+`
+
+const RightContact = styled.div`
+    font-size: 1.5rem;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+`
