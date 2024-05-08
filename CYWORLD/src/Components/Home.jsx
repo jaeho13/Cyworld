267bdb6ad930fe437ddfc738a388ee9a63d47c93
jaeho13 <jaehooo13@naver.com>
2024-05-08 23:25:00 +0900

    0508 홈페이지 버튼 import 구현

--- CYWORLD/src/Components/Home.jsx
+++ CYWORLD/src/Components/Home.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import UseNavigation from "./UseNavigation";
+import { RightContact, RightContactFlexWhite, RightHome, RightHomeFlexBlack, RightProfile, RightProfileFlexWhite, RightProject, RightProjectFlexBlack, RightProjectFlexWhite } from "./button/Button";
 
 const Home = () => {
 
@@ -75,21 +76,21 @@ const Home = () => {
                                     </Center>
 
                                     <Right>
-                                        <RightHomeFlex>
-                                            <RightHome onClick={goToHome} >Home</RightHome>
-                                        </RightHomeFlex>
+                                        <RightHomeFlexBlack>
+                                            <RightHome onClick={goToHome}>Home</RightHome>
+                                        </RightHomeFlexBlack>
 
-                                        <RightProfileFlex>
-                                            <RightProfile onClick={goToProfile} >Profile</RightProfile>
-                                        </RightProfileFlex>
+                                        <RightProfileFlexWhite>
+                                            <RightProfile onClick={goToProfile}>Profile</RightProfile>
+                                        </RightProfileFlexWhite>
 
-                                        <RightProjectFlex>
-                                            <RightProject onClick={goToProject} >Project</RightProject>
-                                        </RightProjectFlex>
+                                        <RightProjectFlexWhite>
+                                            <RightProject onClick={goToProject}>Project</RightProject>
+                                        </RightProjectFlexWhite>
 
-                                        <RightContactFlex>
-                                            <RightContact onClick={goToContact} >Contact</RightContact>
-                                        </RightContactFlex>
+                                        <RightContactFlexWhite>
+                                            <RightContact onClick={goToContact}>Contact</RightContact>
+                                        </RightContactFlexWhite>
                                     </Right>
                                 </RightBind>
                             </BoardBind>
@@ -390,92 +391,4 @@ const CenterMiniroomImageArea = styled.img`
 const Right = styled.div`
     width: 10%;
     height: 75vh;
-`
-
-const RightHomeFlex = styled.div`
-    width: 97%;
-    height: 5vh;
-    border: 2px solid grey;
-    border-top-right-radius: 8px;
-    border-bottom-right-radius: 8px;
-    cursor: pointer;
-    color: black;
-    background-color: white;
-    margin-top: 5vh;
-    padding-left: 3%;
-    display: flex;
-    align-items: center;
-`
-
-const RightHome = styled.div`
-    font-size: 1.5rem;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-`
-
-const RightProfileFlex = styled.div`
-    width: 97%;
-    height: 5vh;
-    border: 2px solid grey;
-    border-top-right-radius: 8px;
-    border-bottom-right-radius: 8px;
-    cursor: pointer;    
-    color: white;
-    background-color: #3B87AB;
-    margin-top: 1vh;
-    padding-left: 3%;
-    display: flex;
-    align-items: center;
-`
-
-const RightProfile = styled.div`
-    font-size: 1.5rem;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-`
-
-const RightProjectFlex = styled.div`
-    width: 97%;
-    height: 5vh;
-    border: 2px solid grey;
-    border-top-right-radius: 8px;
-    border-bottom-right-radius: 8px;
-    cursor: pointer;
-    color: white;
-    background-color: #3B87AB;
-    margin-top: 1vh;
-    padding-left: 3%;
-    display: flex;
-    align-items: center;
-`
-
-const RightProject = styled.div`
-    font-size: 1.5rem;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-`
-
-const RightContactFlex = styled.div`
-    width: 97%;
-    height: 5vh;
-    border: 2px solid grey;
-    border-top-right-radius: 8px;
-    border-bottom-right-radius: 8px;
-    cursor: pointer;
-    color: white;
-    background-color: #3B87AB;
-    margin-top: 1vh;
-    padding-left: 3%;
-    display: flex;
-    align-items: center;
-`
-
-const RightContact = styled.div`
-    font-size: 1.5rem;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
 `
