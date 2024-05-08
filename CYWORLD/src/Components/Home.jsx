69797bf4699e4760d4f96844c78858498dbe34a9
jaeho13 <jaehooo13@naver.com>
2024-05-09 02:12:52 +0900

    "0509 배경 컴포넌트화"

--- CYWORLD/src/Components/Home.jsx
+++ CYWORLD/src/Components/Home.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import styled from "styled-components";
 import UseNavigation from "./UseNavigation";
-import { RightContact, RightContactFlexWhite, RightHome, RightHomeFlexBlack, RightProfile, RightProfileFlexWhite, RightProject, RightProjectFlexBlack, RightProjectFlexWhite } from "./button/Button";
+import { RightContact, RightContactFlexWhite, RightHome, RightHomeFlexBlack, RightProfile, RightProfileFlexWhite, RightProject, RightProjectFlexWhite } from "./button/Button";
+import { BackArea, BackBorder, BackColor, BackgroundImage } from "./background/Background";
 
 const Home = () => {
 
@@ -103,51 +104,6 @@ const Home = () => {
 };
 
 export default Home;
-
-const BackgroundImage = styled.div`
-    background-color: #a3a3a3;
-    background-image: url(/background/pattern.png);
-    background-size: 100px;
-    position: fixed;
-    left: 0;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
-
-const BackArea = styled.div`
-    width: 70%;
-    height: 85vh;
-    border: 2px solid #738186;
-    border-radius: 30px;
-    background-color: #b4d1da;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
-
-const BackBorder = styled.div`
-    width: 97%;
-    height: 80vh;
-    border: 3.5px dashed white;
-    border-radius: 15px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
-
-const BackColor = styled.div`
-    width: 97%;
-    height: 75vh;
-    border: 2px solid #d4d4d4;
-    border-radius: 1rem;
-    background-color: #d4d4d4;
-    display: flex;
-    align-items: center;
-`
 
 const BoardBind = styled.div`
     width: 100%;
---
+++ CYWORLD/src/Components/background/Background.jsx
@@ -0,0 +1,47 @@
+import styled from "styled-components";
+
+
+export const BackgroundImage = styled.div`
+    background-color: #a3a3a3;
+    background-image: url(/background/pattern.png);
+    background-size: 100px;
+    position: fixed;
+    left: 0;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const BackArea = styled.div`
+    width: 70%;
+    height: 85vh;
+    border: 2px solid #738186;
+    border-radius: 30px;
+    background-color: #b4d1da;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const BackBorder = styled.div`
+    width: 97%;
+    height: 80vh;
+    border: 3.5px dashed white;
+    border-radius: 15px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const BackColor = styled.div`
+    width: 97%;
+    height: 75vh;
+    border: 2px solid #d4d4d4;
+    border-radius: 1rem;
+    background-color: #d4d4d4;
+    display: flex;
+    align-items: center;
+`
