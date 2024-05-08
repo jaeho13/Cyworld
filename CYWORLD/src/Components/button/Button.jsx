3327310873f385fb4a143837d0f2f9a4fa3f9014
jaeho13 <jaehooo13@naver.com>
2024-05-08 23:26:55 +0900

    "0508 홈페이지 버튼 import 구현"

--- CYWORLD/src/Components/button/Button.jsx
+++ CYWORLD/src/Components/button/Button.jsx
@@ -1,16 +1,7 @@
 import styled from "styled-components";
 
-const Button = () => {
-    return (
-        <>
-        </>
-    )
-}
 
-export default Button;
-
-
-const RightHomeFlex = styled.div`
+export const RightHomeFlexBlack = styled.div`
     width: 97%;
     height: 5vh;
     border: 2px solid grey;
@@ -19,42 +10,55 @@ const RightHomeFlex = styled.div`
     cursor: pointer;
     color: black;
     background-color: white;
+    /* color: ${props => props.textColor || "black"};
+    background-color: ${props => props.color || "white"}; */
     margin-top: 5vh;
     padding-left: 3%;
     display: flex;
     align-items: center;
 `
 
-const RightHome = styled.div`
+export const RightHomeFlexWhite = styled.div`
+    width: 97%;
+    height: 5vh;
+    border: 2px solid grey;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+    cursor: pointer;
+    color: white;
+    background-color: #3B87AB;
+    /* color: ${props => props.textColor || "black"};
+    background-color: ${props => props.color || "white"}; */
+    margin-top: 5vh;
+    padding-left: 3%;
+    display: flex;
+    align-items: center;
+`
+
+
+export const RightHome = styled.div`
     font-size: 1.5rem;
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
 `
 
-const RightProfileFlex = styled.div`
+export const RightProfileFlexBlack = styled.div`
     width: 97%;
     height: 5vh;
     border: 2px solid grey;
     border-top-right-radius: 8px;
     border-bottom-right-radius: 8px;
-    cursor: pointer;    
-    color: white;
-    background-color: #3B87AB;
+    cursor: pointer;
+    color: black;
+    background-color: white;
     margin-top: 1vh;
     padding-left: 3%;
     display: flex;
     align-items: center;
 `
 
-const RightProfile = styled.div`
-    font-size: 1.5rem;
-    white-space: nowrap;
-    overflow: hidden;
-    text-overflow: ellipsis;
-`
-
-const RightProjectFlex = styled.div`
+export const RightProfileFlexWhite = styled.div`
     width: 97%;
     height: 5vh;
     border: 2px solid grey;
@@ -69,14 +73,30 @@ const RightProjectFlex = styled.div`
     align-items: center;
 `
 
-const RightProject = styled.div`
+export const RightProfile = styled.div`
     font-size: 1.5rem;
     white-space: nowrap;
     overflow: hidden;
     text-overflow: ellipsis;
 `
 
-const RightContactFlex = styled.div`
+export const RightProjectFlexBlack = styled.div`
+    width: 97%;
+    height: 5vh;
+    border: 2px solid grey;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+    cursor: pointer;
+    color: black;
+    background-color: white;
+    margin-top: 1vh;
+    padding-left: 3%;
+    display: flex;
+    align-items: center;
+`
+
+
+export const RightProjectFlexWhite = styled.div`
     width: 97%;
     height: 5vh;
     border: 2px solid grey;
@@ -91,7 +111,44 @@ const RightContactFlex = styled.div`
     align-items: center;
 `
 
-const RightContact = styled.div`
+export const RightProject = styled.div`
+    font-size: 1.5rem;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+`
+
+export const RightContactFlexBlack = styled.div`
+    width: 97%;
+    height: 5vh;
+    border: 2px solid grey;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+    cursor: pointer;
+    color: black;
+    background-color: white;
+    margin-top: 1vh;
+    padding-left: 3%;
+    display: flex;
+    align-items: center;
+`
+
+export const RightContactFlexWhite = styled.div`
+    width: 97%;
+    height: 5vh;
+    border: 2px solid grey;
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+    cursor: pointer;
+    color: white;
+    background-color: #3B87AB;
+    margin-top: 1vh;
+    padding-left: 3%;
+    display: flex;
+    align-items: center;
+`
+
+export const RightContact = styled.div`
     font-size: 1.5rem;
     white-space: nowrap;
     overflow: hidden;
