a4957a829028c8c2c5b955293194b3fc0f2ca916
jaeho13 <jaehooo13@naver.com>
2024-09-07 02:24:21 +0900

    "0907 홈화면 코드 리팩토링"

--- CYWORLD/src/Components/Home.jsx
+++ CYWORLD/src/Components/Home.jsx
@@ -1,8 +1,7 @@
-import React from "react";
+import React from 'react';
 import styled from "styled-components";
-import UseNavigation from "./UseNavigation";
-import { RightContact, RightContactFlexWhite, RightHome, RightHomeFlexBlack, RightProfile, RightProfileFlexWhite, RightProject, RightProjectFlexWhite } from "./button/Button";
-import { BackArea, BackBorder, BackColor, BackgroundImage } from "./background/Background";
+import Background from './background/Background';
+import PageButton from "./button/PageButton";
 
 const Home = () => {
 
@@ -12,96 +11,73 @@ const Home = () => {
         }
     };
 
-    const { goToHome, goToProfile, goToProject, goToContact } = UseNavigation();
-
     return (
         <>
-            <BackgroundImage>
-                <BackArea>
-                    <BackBorder>
-                        <BackColor>
-                            <BoardBind>
-                                <Left>
-                                    <LeftTopFlex>
-                                        <LeftTop>Today</LeftTop>
-                                        <VisitCount>11</VisitCount>
-                                        <LeftTop>| TOTAL 1113</LeftTop>
-                                    </LeftTopFlex>
-                                    {/* 수정 글씨 반응형 */}
-                                    <LeftProfile>
-                                        <LeftProfileImage src="/background/Picture.png" alt="profile" />
-                                    </LeftProfile>
-                                    <LeftInfoTitle>
-                                        Introduce
-                                    </LeftInfoTitle>
-                                    <LeftInfo>
-                                        프론트엔드 개발자를 꿈꾸는 이재호입니다.<br />
-                                        사용자 경험을 고려한 직관적이고 효율적인 인터페이스를 디자인하고 개발하는 것에 관심이 많습니다.<br />
-                                    </LeftInfo>
-                                    <LeftLinkTitle>
-                                        Link
-                                    </LeftLinkTitle>
-                                    <LeftLink>
-                                        <LeftLinkSelect onChange={(e) => handleLinkClick(e.target.value)}>
-                                            <option value="사이트">깃허브 및 블로그</option>
-                                            <option>https://github.com/jaeho13</option>
-                                            <option>https://velog.io/@jaehooo13</option>
-                                        </LeftLinkSelect>
-                                    </LeftLink>
-                                </Left>
+            <Background>
+                <BoardBind>
+                    <Left>
+                        <LeftTopFlex>
+                            <LeftTop>Today</LeftTop>
+                            <VisitCount>11</VisitCount>
+                            <LeftTop>| TOTAL 1113</LeftTop>
+                        </LeftTopFlex>
+                        <LeftProfile>
+                            <LeftProfileImage src="/background/Picture.png" alt="profile" />
+                        </LeftProfile>
+                        <LeftInfoTitle>
+                            Introduce
+                        </LeftInfoTitle>
+                        <LeftInfo>
+                            프론트엔드 개발자를 꿈꾸는 이재호입니다.<br />
+                            사용자 경험을 고려한 직관적이고 효율적인 인터페이스를 디자인하고 개발하는 것에 관심이 많습니다.<br />
+                        </LeftInfo>
+                        <LeftLinkTitle>
+                            Link
+                        </LeftLinkTitle>
+                        <LeftLink>
+                            <LeftLinkSelect onChange={(e) => handleLinkClick(e.target.value)}>
+                                <option value="사이트">깃허브 및 블로그</option>
+                                <option>https://github.com/jaeho13</option>
+                                <option>https://velog.io/@jaehooo13</option>
+                            </LeftLinkSelect>
+                        </LeftLink>
+                    </Left>
 
-                                <RightBind>
-                                    <Center>
-                                        <CenterTop>
-                                            <CenterTopSiteFlex>
-                                                <CenterTopSite> 이재호의 미니홈피</CenterTopSite>
-                                            </CenterTopSiteFlex>
-                                            <CenterTopLinkFlex>
-                                                <CenterTopLink>
-                                                    <a href="https://jaeho13.github.io">https://jaeho13.github.io</a>
-                                                </CenterTopLink>
-                                            </CenterTopLinkFlex>
-                                        </CenterTop>
-                                        <CenterCommitState>
-                                            Commit States
-                                        </CenterCommitState>
-                                        <CenterCommitImage>
-                                            <CenterCommitImageArea src="https://ghchart.rshah.org/jaeho13" />
-                                        </CenterCommitImage>
-                                        <CenterMiniroom>
-                                            Miniroom
-                                        </CenterMiniroom>
-                                        <CenterMiniroomImage>
-                                            <CenterMiniroomImageArea src="/background/study.png" alt="study" />
-                                        </CenterMiniroomImage>
-                                    </Center>
+                    <RightBind>
+                        <Center>
+                            <CenterTop>
+                                <CenterTopSiteFlex>
+                                    <CenterTopSite> 이재호의 미니홈피</CenterTopSite>
+                                </CenterTopSiteFlex>
+                                <CenterTopLinkFlex>
+                                    <CenterTopLink>
+                                        <a href="https://jaeho13.github.io">https://jaeho13.github.io</a>
+                                    </CenterTopLink>
+                                </CenterTopLinkFlex>
+                            </CenterTop>
+                            <CenterCommitState>
+                                Commit States
+                            </CenterCommitState>
+                            <CenterCommitImage>
+                                <CenterCommitImageArea src="https://ghchart.rshah.org/jaeho13" />
+                            </CenterCommitImage>
+                            <CenterMiniroom>
+                                Miniroom
+                            </CenterMiniroom>
+                            <CenterMiniroomImage>
+                                <CenterMiniroomImageArea src="/background/study.png" alt="study" />
+                            </CenterMiniroomImage>
+                        </Center>
 
-                                    <Right>
-                                        <RightHomeFlexBlack>
-                                            <RightHome onClick={goToHome}>Home</RightHome>
-                                        </RightHomeFlexBlack>
-
-                                        <RightProfileFlexWhite>
-                                            <RightProfile onClick={goToProfile}>Profile</RightProfile>
-                                        </RightProfileFlexWhite>
-
-                                        <RightProjectFlexWhite>
-                                            <RightProject onClick={goToProject}>Project</RightProject>
-                                        </RightProjectFlexWhite>
-
-                                        <RightContactFlexWhite>
-                                            <RightContact onClick={goToContact}>Contact</RightContact>
-                                        </RightContactFlexWhite>
-                                    </Right>
-                                </RightBind>
-                            </BoardBind>
-                        </BackColor>
-                    </BackBorder>
-                </BackArea>
-            </BackgroundImage>
+                        <Right>
+                            <PageButton />
+                        </Right>
+                    </RightBind>
+                </BoardBind>
+            </Background>
         </>
-    );
-};
+    )
+}
 
 export default Home;
 
