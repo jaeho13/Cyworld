c9e65c5ba0db1ef3b12fce3c75e7685ddfb36601
jaeho13 <jaehooo13@naver.com>
2024-09-12 01:50:51 +0900

    "0912 프로젝트 설명란 생성"

---
+++ CYWORLD/src/Components/ProjectInfo.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styled from "styled-components"
+
+
+const ProjectInfo = () => {
+    return (
+        <>
+            <TextBind>
+
+            </TextBind>
+        </>
+    )
+}
+
+export default ProjectInfo;
+
+const TextBind = styled.div`
+    width: 40%;
+    height: 24vh;
+    border: 2px solid #A3A3A3;
+    border-radius: 16px;
+    overflow: auto;
+    overflow-x: hidden;
+    background-color: rgba(255, 255, 255, 0.4);
+    padding-left: 1%;
+    padding-top: 1%;
+`
