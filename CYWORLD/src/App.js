ad81c157143bce2a7c31db5421dd2be76c00cf86
jaeho13 <jaehooo13@naver.com>
2024-09-13 01:31:53 +0900

    "0913 프로젝트 구조 수정"

--- CYWORLD/src/App.js
+++ CYWORLD/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Home from "./Components/Home";
-import Profile from "./Components/Profile";
-import Project from "./Components/Project";
-import Contact from "./Components/Contact";
+import Home from "./Components/home/Home";
+import Profile from "./Components/profile/Profile";
+import Project from "./Components/project/Project";
+import Contact from "./Components/contact/Contact";
 
 function App() {
   return (
