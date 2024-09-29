078096abfc9782bec58b3689a8ce60649d54a3d7
jaeho13 <jaehooo13@naver.com>
2024-09-29 22:49:35 +0900

    "0929 Contact 페이지 스타일 분리"

---
+++ CYWORLD/src/Components/contact/styles/ContactStyle.jsx
@@ -0,0 +1,119 @@
+import styled from "styled-components"
+
+export const FiledBind = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+`
+
+export const FiledLeft = styled.div`
+    width: 90%;
+    height: 76vh;
+    display: flex;
+    flex-direction: column;
+    border-radius: 16px;
+    background-color: rgba(255, 255, 255, 0.4);
+`
+
+export const ContactTitle = styled.div`
+    width: 97%;
+    height: 3vh;
+    margin-top: 1vh;
+    color: #3B87AB;
+    font-weight: bold;
+    font-size: 1.5rem;
+    padding-left: 3%;
+    display: flex;
+    align-items: center;
+`
+
+export const BoardBind = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`
+
+export const BoardTop = styled.div`
+    width: 95%;
+    height: 15vh;
+    margin-top: 1vh;
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    background-color: white;
+    border-top-left-radius: 16px;
+    border-top-right-radius: 16px;
+`
+
+export const IconBorder = styled.div`
+    width: 90px;
+    height: 90px;
+    border: 2px solid black;
+    border-radius: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const ContactIcon = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+    border-radius: 16px;
+`
+
+export const BoardCenterBind = styled.div`
+    width: 95%;
+    display: flex;
+    flex-direction: row;
+`
+
+export const BoardCenter = styled.div`
+    width: 50%;
+    height: 7vh;
+    background-color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 1.5rem;
+    color: #3B87AB;
+    font-weight: bold;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+`
+
+export const BoardBottom = styled.div`
+    width: 95%;
+    height: 48vh;
+    background-color: white;
+    border-bottom-left-radius: 16px;
+    border-bottom-right-radius: 16px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+
+export const ImgBorder = styled.div`
+    width: 90%;
+    height: 90%;
+    border-radius: 16px;
+`
+
+export const EndImg = styled.img`
+    width: 100%;
+    height: 100%;
+    object-fit: fill;
+    border-radius: 16px;
+`
+
+export const FiledRight = styled.div`
+    width: 7.5%;
+    height: 73vh;
+`
+
+export const BtnDown = styled.div`
+    padding-top: 17px;
+`
