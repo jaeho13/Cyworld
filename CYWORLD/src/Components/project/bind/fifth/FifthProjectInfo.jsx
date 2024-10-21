fc1d1eb3257d113f8a2c2ec97196ec212400ba2e
jaeho13 <jaehooo13@naver.com>
2024-10-22 01:10:42 +0900

    "1022 Project 역할 추가"

--- CYWORLD/src/Components/project/bind/fifth/FifthProjectInfo.jsx
+++ CYWORLD/src/Components/project/bind/fifth/FifthProjectInfo.jsx
@@ -18,10 +18,16 @@ const FifthProjectInfo = () => {
                 - 기존 학습 내용 복습 및 정리 <br />
                 - 사용해 보지 않은 신규 라이브러리 사용 <br /> <br />
 
-                3. 사용 기술 <br />
+                3. 역할 <br />
+                - 기획 및 디자인 <br />
+                - Frontend <br />
+                - RESTful API를 이용한 React, Spring 게시판 CRUD 구현 <br />
+                - 차트를 이용한 대시보드 구현 <br /> <br />
+
+                4. 사용 기술 <br />
                 - React, styled-components, axios, RESTful API, react-chartjs <br /> <br />
 
-                4. 구현 내용 <br />
+                5. 구현 내용 <br />
                 - axios 통신을 이용한 비동기 통신 <br />
                 - 도서 검색 기능 구현 <br />
                 - RESTful API를 이용한 도서, 사용자 관리 CRUD <br />
