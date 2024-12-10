dcbcc24d843304c77f92cb80b781ecc84d5a5754
jaeho13 <jaehooo13@naver.com>
2024-12-11 00:52:52 +0900

    테스트

--- CYWORLD/src/App.js
+++ CYWORLD/src/App.js
@@ -1,22 +1,21 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
 import Home from "./Components/home/Home";
 import Profile from "./Components/profile/Profile";
 import Project from "./Components/project/Project";
 import Contact from "./Components/contact/Contact";
-import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <HashRouter>
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/project" element={<Project />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </>
   );
 }
