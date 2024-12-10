e5f5f755a6d6164e27e951ade3155eaa1dcc1bf2
jaeho13 <jaehooo13@naver.com>
2024-12-11 00:08:55 +0900

    "1211 gh-pages 테스트"

--- CYWORLD/src/App.js
+++ CYWORLD/src/App.js
@@ -4,18 +4,19 @@ import Home from "./Components/home/Home";
 import Profile from "./Components/profile/Profile";
 import Project from "./Components/project/Project";
 import Contact from "./Components/contact/Contact";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <BrowserRouter basename={process.env.PUBLIC_URL}>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/project" element={<Project />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
