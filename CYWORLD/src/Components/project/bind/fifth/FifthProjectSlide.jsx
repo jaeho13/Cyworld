e5b748e41a490082eb9ecea656f05fe453562f72
jaehooo13 <jaehooo13@naver.com>
2025-04-24 23:27:18 +0900

    프로젝트 내용 변경

--- CYWORLD/src/Components/project/bind/fifth/FifthProjectSlide.jsx
+++ CYWORLD/src/Components/project/bind/fifth/FifthProjectSlide.jsx
@@ -1,37 +1,35 @@
 import React from "react";
 import Slider from "react-slick";
-import 'slick-carousel/slick/slick.css';
-import 'slick-carousel/slick/slick-theme.css';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import ProjectImage from "../../ProjectImage";
 import { SliderBorder } from "../styles/BindStyle";
 
 const FifthProjectSlide = () => {
+  var settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    arrows: false,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  };
 
-    var settings = {
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        draggable: true,
-        arrows: false,
-        autoplay: true,
-        autoplaySpeed: 2000,
-    };
-
-    return (
-        <>
-            <SliderBorder>
-                <Slider {...settings}>
-                    <ProjectImage imgSrc="/background/l_page1.png" />
-                    <ProjectImage imgSrc="/background/l_page2.png" />
-                    <ProjectImage imgSrc="/background/l_page3.png" />
-                    <ProjectImage imgSrc="/background/l_page4.png" />
-
-                </Slider>
-            </SliderBorder>
-        </>
-    )
-}
+  return (
+    <>
+      <SliderBorder>
+        <Slider {...settings}>
+          {/* <ProjectImage imgSrc="/background/l_page1.png" /> */}
+          <ProjectImage imgSrc="/background/l_page2.png" />
+          <ProjectImage imgSrc="/background/l_page3.png" />
+          <ProjectImage imgSrc="/background/l_page4.png" />
+        </Slider>
+      </SliderBorder>
+    </>
+  );
+};
 
 export default FifthProjectSlide;
