d358c0d4086955e9d6e02aa55bcdd0863faf16d8
jaehooo13 <jaehooo13@naver.com>
2025-04-24 23:24:54 +0900

    해시 라우터로 변경

--- CYWORLD/src/App.js
+++ CYWORLD/src/App.js
@@ -1,21 +1,23 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import { Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./Components/home/Home";
 import Profile from "./Components/profile/Profile";
 import Project from "./Components/project/Project";
 import Contact from "./Components/contact/Contact";
+import GlobalStyle from "./Components/globalStyle/GlobalStyle";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
+      <GlobalStyle />
+      <HashRouter>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/project" element={<Project />} />
           <Route path="/contact" element={<Contact />} />
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </>
   );
 }
