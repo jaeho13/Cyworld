47b6851aeb93358c8f61f87a0e01c7226fa411f6
jaehooo13 <jaehooo13@naver.com>
2025-04-26 23:56:51 +0900

    콘솔 배경 색상 에러 수정

--- CYWORLD/src/Components/button/PageButton.jsx
+++ CYWORLD/src/Components/button/PageButton.jsx
@@ -1,52 +1,55 @@
 import React from "react";
 import UseNavigation from "../UseNavigation";
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 import { TestBtn } from "./styles/PageButtonStyle";
 
 const PageButton = (props) => {
-    const { goToHome, goToProfile, goToProject, goToContact } = UseNavigation();
-    const location = useLocation();
+  const { goToHome, goToProfile, goToProject, goToContact } = UseNavigation();
+  const location = useLocation();
 
-    const handleButtonClick = () => {
-        switch (props.name) {
-            case "Home":
-                goToHome();
-                break;
-            case "Profile":
-                goToProfile();
-                break;
-            case "Project":
-                goToProject();
-                break;
-            case "Contact":
-                goToContact();
-                break;
-            default:
-                break;
-        }
-    };
+  const handleButtonClick = () => {
+    switch (props.name) {
+      case "Home":
+        goToHome();
+        break;
+      case "Profile":
+        goToProfile();
+        break;
+      case "Project":
+        goToProject();
+        break;
+      case "Contact":
+        goToContact();
+        break;
+      default:
+        break;
+    }
+  };
 
-    const getBackgroundColor = () => {
-        const currentPath = location.pathname;
+  const getBackgroundColor = () => {
+    const currentPath = location.pathname;
 
-        if (props.name === "Home" && currentPath === "/") {
-            return "white";
-        } else if (props.name === "Profile" && currentPath === "/profile") {
-            return "white";
-        } else if (props.name === "Project" && currentPath === "/project") {
-            return "white";
-        } else if (props.name === "Contact" && currentPath === "/contact") {
-            return "white";
-        } else {
-            return "#3B87AB";
-        }
-    };
+    if (props.name === "Home" && currentPath === "/") {
+      return "white";
+    } else if (props.name === "Profile" && currentPath === "/profile") {
+      return "white";
+    } else if (props.name === "Project" && currentPath === "/project") {
+      return "white";
+    } else if (props.name === "Contact" && currentPath === "/contact") {
+      return "white";
+    } else {
+      return "#3B87AB";
+    }
+  };
 
-    return (
-        <TestBtn onClick={handleButtonClick} backgroundColor={getBackgroundColor()}>
-            {props.name}
-        </TestBtn>
-    );
+  return (
+    <TestBtn
+      onClick={handleButtonClick}
+      $backgroundColor={getBackgroundColor()}
+    >
+      {props.name}
+    </TestBtn>
+  );
 };
 
 export default PageButton;
--- CYWORLD/src/Components/button/styles/PageButtonStyle.jsx
+++ CYWORLD/src/Components/button/styles/PageButtonStyle.jsx
@@ -1,22 +1,22 @@
 import styled from "styled-components";
 
 export const TestBtn = styled.div`
-    width: 97%;
-    height: 5vh;
-    line-height: 5vh;
-    border: 2px solid grey;
-    border-top-right-radius: 8px;
-    border-bottom-right-radius: 8px;
-    cursor: pointer;
-    color: ${props => props.backgroundColor === "white" ? "black" : "white"};
-    background-color: ${props => props.backgroundColor};
-    margin-top: 1vh;
-    padding-left: 3%;
-    /* display: flex; */
-    align-items: center;
-    /* font-size: 1.3rem; */
-    font-size: 20px;
-    /* white-space: nowrap; */
-    overflow: hidden;
-    text-overflow: ellipsis;
+  width: 97%;
+  height: 5vh;
+  line-height: 5vh;
+  border: 2px solid grey;
+  border-top-right-radius: 8px;
+  border-bottom-right-radius: 8px;
+  cursor: pointer;
+  color: ${(props) => (props.$backgroundColor === "white" ? "black" : "white")};
+  background-color: ${(props) => props.$backgroundColor};
+  margin-top: 1vh;
+  padding-left: 3%;
+  /* display: flex; */
+  align-items: center;
+  /* font-size: 1.3rem; */
+  font-size: 20px;
+  /* white-space: nowrap; */
+  overflow: hidden;
+  text-overflow: ellipsis;
 `;
